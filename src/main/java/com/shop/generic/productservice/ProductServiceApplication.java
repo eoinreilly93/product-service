2b0d1a74e574fc6b32c4d0843f98fbae3f4d282c
eoinreilly <eoindreilly@gmail.com>
2024-10-06 22:57:49 +0100

    refactor: Separate out kafka autoconfigurations for project
    Moves kafka consumer and producer configurations to their own autoconfigurations within the common project. This allows downstream projects that have the dependency to exclude the consumer or producer (or both) should they not require that functionality in the project.

--- src/main/java/com/shop/generic/productservice/ProductServiceApplication.java
+++ src/main/java/com/shop/generic/productservice/ProductServiceApplication.java
@@ -1,10 +1,13 @@
 package com.shop.generic.productservice;
 
+import com.shop.generic.common.CommonKafkaConsumerAutoConfiguration;
+import com.shop.generic.common.CommonKafkaProducerAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {CommonKafkaProducerAutoConfiguration.class,
+        CommonKafkaConsumerAutoConfiguration.class})
 @EntityScan("com.shop.generic.common.entities")
 public class ProductServiceApplication {
 
