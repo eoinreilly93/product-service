b9a5ef321c8014594edecf5c9548a78cedd910e6
eoinreilly <eoindreilly@gmail.com>
2024-10-07 21:15:29 +0100

    refactor: Define products table name instead of letting it be inferred

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,9 @@
-CREATE TABLE product (
-   product_id SERIAL NOT NULL,
-   name VARCHAR(255) NOT NULL,
-   price DECIMAL NOT NULL,
-   stock_status VARCHAR(20) NOT NULL,
-   stock_count INT NOT NULL,
-   CONSTRAINT pk_product PRIMARY KEY (product_id)
+CREATE TABLE products
+(
+    product_id   SERIAL       NOT NULL,
+    name         VARCHAR(255) NOT NULL,
+    price        DECIMAL      NOT NULL,
+    stock_status VARCHAR(20)  NOT NULL,
+    stock_count  INT          NOT NULL,
+    CONSTRAINT pk_product PRIMARY KEY (product_id)
 );
