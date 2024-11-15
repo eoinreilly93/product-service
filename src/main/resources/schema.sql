99eb052ef86ea1e2d4d889521cae766e36c9f52d
eoinreilly <eoindreilly@gmail.com>
2024-11-15 13:35:38 +0000

    refactor: Use a custom sequence for the database ids
    - Allows us to control the id value better via a sequence

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,6 +1,8 @@
+CREATE SEQUENCE IF NOT EXISTS product_id_seq START WITH 1 INCREMENT BY 1;
+
 CREATE TABLE products
 (
-    product_id   SERIAL       NOT NULL,
+    product_id BIGINT NOT NULL,
     name         VARCHAR(255) NOT NULL,
     price        DECIMAL      NOT NULL,
     stock_status VARCHAR(20)  NOT NULL,
